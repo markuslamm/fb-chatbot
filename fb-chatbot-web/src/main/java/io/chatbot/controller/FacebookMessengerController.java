7f43fc031d64e05434e5e40b97e72c46d1109498
Markus Lamm <markus.lamm@googlemail.com>
2016-05-06 21:28:28 +0200

    extract fb verify token property

--- fb-chatbot-web/src/main/java/io/chatbot/controller/FacebookMessengerController.java
+++ fb-chatbot-web/src/main/java/io/chatbot/controller/FacebookMessengerController.java
@@ -2,6 +2,7 @@ package io.chatbot.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,17 @@ public final class FacebookMessengerController {
 
     private static final Logger LOG = LoggerFactory.getLogger(FacebookMessengerController.class);
 
+    private final String fbVerifyToken;
+
+    public FacebookMessengerController(@Value("${fb.verifytoken}") String fbVerifyToken) {
+        this.fbVerifyToken = fbVerifyToken;
+    }
+
     @RequestMapping(value = "/hook", method = RequestMethod.GET)
     public String fbMessengerHook(@RequestParam(name = "hub.verify_token") final String verifyToken,
                                   @RequestParam(name = "hub.challenge") final String challenge) {
         LOG.info("Received Facebook request, hub.verify_token={}, hub.challenge={}", verifyToken, challenge);
-        if(!"1234567890".equals(verifyToken)) {
+        if(!fbVerifyToken.equals(verifyToken)) {
             throw new RuntimeException("invalid token");
         }
         return challenge;
