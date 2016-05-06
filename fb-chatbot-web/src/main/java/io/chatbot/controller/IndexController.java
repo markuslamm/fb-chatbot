ffa4e4c2109ababaf658ffea05bf777d2f4a7369
Markus Lamm <markus.lamm@googlemail.com>
2016-05-06 19:49:42 +0200

    temp remove thymeleaf

--- fb-chatbot-web/src/main/java/io/chatbot/controller/IndexController.java
+++ fb-chatbot-web/src/main/java/io/chatbot/controller/IndexController.java
@@ -1,18 +1,14 @@
 package io.chatbot.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-
-@Controller
+@RestController
 public final class IndexController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String clientListPage(final Model model) {
-        model.addAttribute("serverTime", LocalDateTime.now());
-        return "index";
+    public String index() {
+        return "response";
     }
 }
