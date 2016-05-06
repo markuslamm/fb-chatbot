2146839feab4b74e86945b99c7eaa25f45979524
Markus Lamm <markus.lamm@googlemail.com>
2016-05-06 21:53:53 +0200

    first message send impl

--- fb-chatbot-web/src/main/java/io/chatbot/controller/FacebookMessengerController.java
+++ fb-chatbot-web/src/main/java/io/chatbot/controller/FacebookMessengerController.java
@@ -3,9 +3,11 @@ package io.chatbot.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import static java.lang.String.format;
 
 @RestController
 public final class FacebookMessengerController {
@@ -13,9 +15,12 @@ public final class FacebookMessengerController {
     private static final Logger LOG = LoggerFactory.getLogger(FacebookMessengerController.class);
 
     private final String fbVerifyToken;
+    private final String fbAccesstoken;
 
-    public FacebookMessengerController(@Value("${fb.verifytoken}") String fbVerifyToken) {
+    public FacebookMessengerController(@Value("${fb.verifytoken}") String fbVerifyToken,
+                                       @Value("${fb.accesstoken}") String fbAccesstoken) {
         this.fbVerifyToken = fbVerifyToken;
+        this.fbAccesstoken = fbAccesstoken;
     }
 
     @RequestMapping(value = "/hook", method = RequestMethod.GET)
@@ -23,14 +28,27 @@ public final class FacebookMessengerController {
                                   @RequestParam(name = "hub.challenge") final String challenge) {
         LOG.info("Received Facebook request, hub.verify_token={}, hub.challenge={}", verifyToken, challenge);
         if(!fbVerifyToken.equals(verifyToken)) {
-            throw new RuntimeException("invalid token");
+            throw new RuntimeException("Invalid token");
         }
         return challenge;
     }
 
     @RequestMapping(value = "/hook", method = RequestMethod.POST)
     public ResponseEntity<Void> handleMessage(@RequestBody final Object msgs) {
-        LOG.info("Received Facebook message event: {}", msgs);
+        LOG.info("Received Facebook message : {}", msgs);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    public void sendMessage(final String recipientId, final String msg) {
+        RestTemplate restTemplate = new RestTemplate();
+        final String url = format("https://graph.facebook.com/v2.6/me/messages?access_token=%s", fbAccesstoken);
+        final String body = format("{\"recipient\": { \"id\": %s }, \"message\": { \"text\":\"hello, world!\" }}", recipientId);
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        final HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        final ResponseEntity<Object> exchange = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Object.class);
+        LOG.info("Send Facebook message: {}", exchange.getStatusCode());
+    }
+
+
 }
