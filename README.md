f931b12836bc552c09e2731f9af84eda6586fe6b
Markus Lamm <markus.lamm@googlemail.com>
2016-05-06 17:12:07 +0200

    setup spring boot app

---
+++ README.md
@@ -0,0 +1,3 @@
+## fb-chatbot
+
+build status: [![Build Status](https://travis-ci.org/markuslamm/fb-chatbot.png?branch=master)](https://travis-ci.org/markuslamm/fb-chatbot)
---
+++ fb-chatbot-web/src/main/java/io/chatbot/ChatbotWebApplication.java
@@ -0,0 +1,12 @@
+package io.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotWebApplication.class, args);
+    }
+}
