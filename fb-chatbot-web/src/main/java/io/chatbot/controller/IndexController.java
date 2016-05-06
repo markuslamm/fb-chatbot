d3db7845bd9381c98356d5a3edd72c6df2859871
Markus Lamm <markus.lamm@googlemail.com>
2016-05-06 19:59:25 +0200

    add thymeleaf controller

--- fb-chatbot-web/src/main/java/io/chatbot/controller/IndexController.java
+++ fb-chatbot-web/src/main/java/io/chatbot/controller/IndexController.java
@@ -1,14 +1,18 @@
 package io.chatbot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.time.LocalDateTime;
+
+@Controller
 public final class IndexController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-        return "response";
+    public String clientListPage(final Model model) {
+        model.addAttribute("serverTime", LocalDateTime.now());
+        return "index";
     }
 }
