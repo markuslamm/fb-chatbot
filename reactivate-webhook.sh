5c6d07d472b0359ffce99f0be3d00f76b8b20c53
Markus Lamm <markus.lamm@googlemail.com>
2016-05-08 16:35:45 +0200

    add script for reactivating webhook after failures

---
+++ reactivate-webhook.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -ik -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAATlCTri1NoBANfXM5nCoHN9sYoBuOVFCNGlnZAOcZBaRH3vV9z46oEEbwbDFeAYrNWROOH1GZARcDcoVPI40BOUSWGYyfCIkBf9HQ7oN0p7rX2iW3uGbjGq8Tw3ZCfXJnDxZAC9rVFGrtlz9BLncecEnbZAQoEALQa2MnU9mPOwZDZD"
